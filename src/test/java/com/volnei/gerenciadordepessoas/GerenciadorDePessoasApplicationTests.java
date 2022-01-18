2aea3019426a6f3cebc5c6c50ca2e0525785ad2a
Volnei Neves <volneinevesfilho@hotmail.com>
2022-01-18 11:25:46 -0300

    First commit

---
+++ src/test/java/com/volnei/gerenciadordepessoas/GerenciadorDePessoasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.volnei.gerenciadordepessoas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadorDePessoasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
